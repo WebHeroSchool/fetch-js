9faa0553ca7a5ee1630abc2038e26d00f94a3eac
space-inside <zhenya.filina@mail.ru>
2019-10-31 16:49:48 +0300

    add alert, if username is absent

--- script.js
+++ script.js
@@ -11,28 +11,33 @@ let name = nameFromUrl(url);
 fetch('https://api.github.com/users/' + name)
     .then(res => res.json())
     .then(json => {
-        let getImage = () => {
-            let photo = document.querySelector('.image');
-            photo.src = json.avatar_url;
-            photo.innerHTML = photo;
+        if(name) {
+            let getImage = () => {
+                let photo = document.querySelector('.image');
+                photo.src = json.avatar_url;
+                photo.innerHTML = photo;
+            }
+            let getLink = () => {
+                let link = document.querySelector('.link');
+                link.href = json.html_url;
+            }
+            let getName = () => {
+                let name = json.name;
+                let h1 = document.querySelector('.title');
+                h1.innerHTML = name;
+            }
+            let getDescription = () => {
+                let p = document.querySelector('.text');
+                let info = json.bio; 
+                p.innerHTML = info;
+            }
+            getImage();
+            getLink();
+            getName();
+            getDescription();
         }
-        let getLink = () => {
-            let link = document.querySelector('.link');
-            link.href = json.html_url;
+        else {
+            alert('Пользователь не найден');
         }
-        let getName = () => {
-            let name = json.name;
-            let h1 = document.querySelector('.title');
-            h1.innerHTML = name;
-        }
-        let getDescription = () => {
-            let p = document.querySelector('.text');
-            let info = json.bio; 
-            p.innerHTML = info;
-        }
-        getImage();
-        getLink();
-        getName();
-        getDescription();
     })
     .catch(err => alert('Информация о пользователе не доступна'))
